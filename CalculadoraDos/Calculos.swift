180bca2a9847b67da1cbc8a2bf32e8fa60397dfb
Luis <luis.fernandomunoz@plexus.es>
2024-04-03 12:42:04 +0200

    Integracion de teclado y calculos a la aplicaciones

---
+++ CalculadoraDos/Calculos.swift
@@ -0,0 +1,8 @@
+//
+//  Calculos.swift
+//  CalculadoraDos
+//
+//  Created by Luis Fernando Sanchez Muñoz on 3/4/24.
+//
+
+import Foundation
---
+++ CalculadoraDos/GeneradorComponentes.swift
@@ -0,0 +1,8 @@
+//
+//  GeneradorComponentes.swift
+//  CalculadoraDos
+//
+//  Created by Luis Fernando Sanchez Muñoz on 3/4/24.
+//
+
+import Foundation
